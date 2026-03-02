5b95b3f3322091d82f92509ba03ac1de2336d5bb
vinaykumar-SRM <lv0667@srmist.edu.in>
2026-03-02 22:14:15 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,10 @@
+/**
+ * OOPSBannerApp - UC1: Print OOPS to Console
+ * @author NithishKumar
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
