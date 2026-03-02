659d6f22893ad04eaebeedcd2abdd63d8f883357
vinaykumar-SRM <lv0667@srmist.edu.in>
2026-03-02 22:08:17 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp - UC4: Array & Loop
+ * @author NithishKumar
+ * @version 4.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        String[] bannerLines = new String[7];
+
+        bannerLines[0] = String.join(" ", " *****  ", " *****  ", "****   ", " *****  ");
+        bannerLines[1] = String.join(" ", "*     * ", "*     * ", "*   *  ", "*       ");
+        bannerLines[2] = String.join(" ", "*     * ", "*     * ", "*   *  ", "*       ");
+        bannerLines[3] = String.join(" ", "*     * ", "*     * ", "****   ", " *****  ");
+        bannerLines[4] = String.join(" ", "*     * ", "*     * ", "*      ", "      * ");
+        bannerLines[5] = String.join(" ", "*     * ", "*     * ", "*      ", "      * ");
+        bannerLines[6] = String.join(" ", " *****  ", " *****  ", "*      ", " *****  ");
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
