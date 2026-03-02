ad734dc0e07dee383eccbd989f6c6c66ac2c735f
vinaykumar-SRM <lv0667@srmist.edu.in>
2026-03-02 22:03:10 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp - UC3: String.join()
+ * @author NithishKumar
+ * @version 3.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", " *****  ", " *****  ", "****   ", " *****  "));
+        System.out.println(String.join(" ", "*     * ", "*     * ", "*   *  ", "*       "));
+        System.out.println(String.join(" ", "*     * ", "*     * ", "*   *  ", "*       "));
+        System.out.println(String.join(" ", "*     * ", "*     * ", "****   ", " *****  "));
+        System.out.println(String.join(" ", "*     * ", "*     * ", "*      ", "      * "));
+        System.out.println(String.join(" ", "*     * ", "*     * ", "*      ", "      * "));
+        System.out.println(String.join(" ", " *****  ", " *****  ", "*      ", " *****  "));
+    }
+}
