376b55d2bdd1b201fc0e9753d72f7a74dfd46c76
vinaykumar-SRM <lv0667@srmist.edu.in>
2026-03-02 22:01:06 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,4 @@
-/**
- * OOPSBannerApp - UC2: Print Banner
- * @author NithishKumar
- * @version 2.0
- */
+
 public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println(" *****  " + " *****  " + "****   " + " *****  ");
